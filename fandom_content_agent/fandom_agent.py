409b000fbf1e44867b387e560c1b7c4f1020197e
Xavier Barrufet <xbarrufetm@gmail.com>
2025-07-29 17:05:41 +0200

    Search url fandom

--- fandom_content_agent/fandom_agent.py
+++ fandom_content_agent/fandom_agent.py
@@ -6,6 +6,7 @@ from langchain.agents import create_react_agent,AgentExecutor
 from langchain import hub
 
 from tools.fandom_tools import get_fandom_url_from_tavity
+from tools.helpers import get_text_from_brackets
 
 load_dotenv()
 
@@ -17,25 +18,27 @@ def lookup_40k_character_url(name:str) -> str:
         temperature=0,
         model_name="gpt-4o-mini")
 
-    template = """dado el personaje {character} que aparece en la wiki fandom quiero que me devuelvas la url de la pagina que contiene su informacion."""
+    template = """dado el personaje {character} que aparece en la wiki fandom quiero que me devuelvas solamente la url de la pagina que contiene su informacion."""
 
     prompt_template = PromptTemplate(
         template=template, input_variables=["character"])
 
     tools_for_agent = [
         Tool(
-            name = "Extrae informacion de las paginas de personajes de la wiki fandom",
+            name = "Buscador de url de personajes en la wiki fandom",
             func=get_fandom_url_from_tavity,
             description = "Utiliza esta herramienta para buscar la página de un personaje en la wiki fandom. La entrada debe ser el nombre del personaje.",
-        )
+        ),
+
+
     ]
 
     react_propmpt = hub.pull("hwchase17/react")
     agent = create_react_agent(llm=llm, tools=tools_for_agent, prompt=react_propmpt)
-    agent_executor = AgentExecutor(agent=agent, tools=tools_for_agent, verbose=True)
+    agent_executor = AgentExecutor(agent=agent, tools=tools_for_agent, verbose=False)
 
     result = agent_executor.invoke(
         input={"input": prompt_template.format_prompt(character=name).to_string()}
     )
     fandom_url = result["output"]
-    return fandom_url
+    return get_text_from_brackets(fandom_url)
--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from fandom_content_agent.fandom_agent import lookup_40k_character_url
-from tools.fandom_tools import search_content_in_fandom
+from fandom_content_agent.fandom_sumarize import summarize_character_information
+from tools.fandom_tools import search_content_in_fandom, get_fandom_url_content_as_markdown, \
+    get_fandom_url_content_as_string
 
 if __name__ == "__main__":
 
     url_base = "https://warhammer40k.fandom.com"
     url = "/es/wiki/Abathar"
 
-    url_char_40k = lookup_40k_character_url("Satoru de Jujutsu Kaisen")
-    print(url_char_40k)
+    url_char = lookup_40k_character_url("Nagi de bluelock")
+    print("url_char: " + url_char)
+    #print("la respues es:" + url_char)
+    #url_char = "https://warhammer40k.fandom.com/wiki/Malcador_the_Sigillite"
+    content = get_fandom_url_content_as_string(url_char)
+    res = summarize_character_information(content)
+    print(res)
+
+    #u1 = "https://warhammer40k.fandom.com/es/wiki/Gal_Vorbak"
+    #u2="https://bluelock.fandom.com/es/wiki/Seishiro_Nagi"
+    #res = get_fandom_url_content(u2)
+    #print(res)
